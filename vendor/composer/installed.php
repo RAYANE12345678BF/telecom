4ed7bf992f758cb7911468f3c0f6bc3f53b972c3
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2025-02-21 00:31:45 +0100

    add new elements and delete old elements

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rayan/dzgrh',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '72cee30f224ba2d243fd4ee07111825572cc2828',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rayan/dzgrh' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '72cee30f224ba2d243fd4ee07111825572cc2828',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
