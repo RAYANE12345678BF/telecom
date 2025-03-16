f522f3c9c766ef7eff5cb1b6f744b898214a7980
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2025-03-16 02:18:22 +0100

    lang

--- dashboard/demands/list.php
+++ dashboard/demands/list.php
@@ -683,7 +683,7 @@ $user_demands = get_user_demands($_SESSION['user_id']);
                             <?= $demand['date_depose'] ?>
                         </td>
                         <td>
-                            <?= $demand['status'] == 'accepted' ? 'accepté' : 'rejecté'; ?>
+                            <?= $demand['status'] == 'accepted' ? 'accepté' : 'rejecé'; ?>
                         </td>
                         <td>
                             <ul>
